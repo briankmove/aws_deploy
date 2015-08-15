4e8c86d0830bfc3889fc8aa2050cbcdc25de5d39
bkim <brian.kim@move.com>
2015-08-14 17:03:22 -0700

    adding script

--- dynamo.py
+++ dynamo.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python
 
 
-import boto3
+#import boto3
 
-#from boto3.session import Session
+from boto.session import Session
 
 # Get the service resource.
 
-#session = Session(region_name='us-west-2')
+session = Session(region_name='us-west-2')
 
 
-dynamodb = boto3.resource('dynamodb')
+dynamodb = session.resource('dynamodb')
 
 
 table = dynamodb.Table('MapiSettings')
 
 
-#response = table.get_item(
-    #Key={
-        #'Type': 'AwsAccountInfo',
-        #'Environment': 'dev'
-    #}
-#)
-#item = response['Item']
-#print(item)
+response = table.get_item(
+    Key={
+        'Type': 'AwsAccountInfo',
+        'Environment': 'dev'
+    }
+)
+item = response['Item']
+print(item)
 
