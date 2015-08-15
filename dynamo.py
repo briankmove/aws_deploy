92827db6e4a656b46b4733cdb6624ea3cf3d8064
bkim <brian.kim@move.com>
2015-08-14 18:03:46 -0700

    updating script

--- dynamo.py
+++ dynamo.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 
-import boto
+import boto.dynamodb
+
+conn = boto.dynamodb.connect_to_region('us-west-2')
 
 #from boto3.session import Session
 
@@ -10,18 +12,18 @@ import boto
 #session = Session(region_name='us-west-2')
 
 
-dynamodb = boto.resource('dynamodb')
+
+#dynamodb = boto.resource('dynamodb')
+
+#table = dynamodb.Table('MapiSettings')
 
 
-table = dynamodb.Table('MapiSettings')
-
-
-response = table.get_item(
-    Key={
-        'Type': 'AwsAccountInfo',
-        'Environment': 'dev'
-    }
-)
-item = response['Item']
-print(item)
+#response = table.get_item(
+    #Key={
+        #'Type': 'AwsAccountInfo',
+        #'Environment': 'dev'
+    #}
+#)
+#item = response['Item']
+#print(item)
 
