8be87582baed5b449df8a24da4eb06b10f44a0c1
bkim <brian.kim@move.com>
2015-10-01 11:13:14 -0700

    adding script

---
+++ deploy/elk_base.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+# Always stop if the script fails
+set -e
+
+delete_temp_ag() {
+    if [ -n $1 ]; then
+        echo "Deleting security group: ${1}"
+        # Delete the temp security group
+        aws ec2 delete-security-group --region ${REGION} --group-id $1
+    fi
+}
+
+# Cleanup if there is an error
+trap 'delete_temp_ag $SG_GROUP_ID_TEMPSSH' ERR
+
+####################################################################
+# Create the base image
+# Script is expected to run from the Jenkins workspace directory
+####################################################################
+#set account permissions on the AMI
+if [ "${Environment}" == "Dev" ]; then
+  ACCNUMBER="425555124585"
+  SUBNETID="subnet-d26ee5b7"
+  VPCID="vpc-5d9e2c38"
+  Environment="dev"
+elif [ "${Environment}" == "QA" ]; then
+  ACCNUMBER="337683724535"
+  SUBNETID="subnet-73961b04"
+  VPCID="vpc-dd1e89b8"
+  Environment="qa"
+else
+  echo "Account not found."
+  exit 1
+fi
+
+echo "ACCNUMBER='${ACCNUMBER}'"
+
+aws sts assume-role --role-arn=arn:aws:iam::${ACCNUMBER}:role/User --role-session-name="userRole" --region=${REGION} > ${WORKSPACE}/credentials.txt
+export AWS_SECRET_ACCESS_KEY=`grep SecretAccessKey credentials.txt |cut -d"\"" -f4`
+export AWS_SECURITY_TOKEN=`grep SessionToken credentials.txt |cut -d"\"" -f4`
+export AWS_ACCESS_KEY_ID=`grep AccessKeyId credentials.txt |cut -d"\"" -f4`
+
+# Get the IP Address of this machine
+JENKINS_SLAVE_IP=`curl -s http://169.254.169.254/latest/meta-data/local-ipv4 2>&1`
+# Create temporary security group to allow Jenkins SSH communication to EC2 instance in RDC
+aws ec2 create-security-group --group-name "mAPI-JenkinsSSH" --vpc-id ${VPCID} --description "mAPI-JenkinsSSH" --region us-west-2 2>&1 | tee sg_create_output.txt
+#Get the group id of above
+export SG_GROUP_ID_TEMPSSH=`grep GroupId sg_create_output.txt | cut -d":" -f 2 | cut -d"\"" -f 2`
+# Add ingress rule to SG
+aws ec2 authorize-security-group-ingress --region us-west-2 --group-id ${SG_GROUP_ID_TEMPSSH} --protocol tcp --port 22 --cidr ${JENKINS_SLAVE_IP}/32
+
+cat <<HERE  > aws.config
+export AWS_ENVIRONMENT=${Environment}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+export AWS_SECURITY_TOKEN=${AWS_SECURITY_TOKEN}
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+HERE
+
+
+
+####################
+# Build The Image
+####################
+echo "packer build -var 'BASEAMI=${BASE_AMI_ID}' -var 'VPCID=${VPCID}' -var 'SUBNETID=${SUBNETID}' ${WORKSPACE}/Mobile-API/deploy/node-packer.json 2>&1 | tee output.txt"
+packer build -color=false \
+  -var "BASEAMI=${BASE_AMI_ID}" \
+  -var "VPCID=${VPCID}" \
+  -var "SUBNETID=${SUBNETID}" \
+  -var "ENVIRONMENT=${Environment}" \
+  -var "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" \
+  -var "AWS_SECURITY_TOKEN=${AWS_SECURITY_TOKEN}" \
+  -var "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" \
+  -var "SG_GROUP_ID_TEMPSSH=${SG_GROUP_ID_TEMPSSH}" \
+    logstash-kibana-packer.json 2>&1 | tee output.txt
+
+# Delete our temporary security group
+delete_temp_ag ${SG_GROUP_ID_TEMPSSH}
+unset SG_GROUP_ID_TEMPSSH
+
+ERROUT=`grep -e "Error" -e "Failed to parse template:" output.txt` || true
+
+if [ -z "${ERROUT}" ]; then
+  echo "exposing AMI..."
+else
+  echo "${ERROUT}: build failed\n"
+  exit 1
+
+
+
+
---
+++ deploy/logstash-kibana-packer-init.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+set -e
+
+sudo su <<HERE
+
+## Install needed packages
+export PATH=$PATH:/usr/local/bin:/sbin
+
+yum update -y aws-cfn-bootstrap
+
+yum install -y gcc-c++ make python-pp git openssl-devel wget
+
+# Make sure the correct user owns the centos directory
+chown -R centos /home/centos
+
+cp cloudwatch-logs.ini /etc
+
+cd ~
+if [ -f awslogs-agent-setup.py ]; then
+    rm awslogs-agent-setup.py
+fi
+wget https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py
+chmod +x ./awslogs-agent-setup.py
+sudo ./awslogs-agent-setup.py -n -r us-west-2 -c /etc/cloudwatch-logs.ini
+
+sudo su <<HERE
+
+if [ -f /home/centos/aws.config ]; then
+    rm /home/centos/aws.config
+fi
+
+HERE
+
+##############################
+# Install Kibana
+##############################
+# Create the nginx User
+echo 'Creating nginx user'
+# uid must be under 500 so it won't get deleted by Ops clean up script
+sudo useradd -u 415 -s /sbin/nologin -m -d /var/lib/nginx -c 'Nginx web server' nginx
+
+cd /opt/
+
+wget https://download.elastic.co/kibana/kibana/kibana-4.0.1-linux-x64.tar.gz
+
+tar xvf kibana-*.tar.gz
+
+mv kibana-*-linux-x64 kibana
+
+sed -i 's/host: "0.0.0.0"/host: "localhost"/g' /opt/kibana/config/kibana.yml
+
+echo 'adding yum repo'
+sudo su <<HERE
+cat >> /etc/systemd/system/kibana4.service << KIBANA
+[Service]
+ExecStart=/opt/kibana/bin/kibana
+Restart=always
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=kibana4
+User=root
+Group=root
+Environment=NODE_ENV=production
+
+[Install]
+WantedBy=multi-user.target
+KIBANA
+
+HERE
+
+systemctl enable kibana4
+
+####################################
+# End : Install Kibana
+####################################
+
+echo "Finished initializing instance."
+exit 0
